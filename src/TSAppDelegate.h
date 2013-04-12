54f3dcb8baf90197ab87579febaf7103e5436b7f
Boris Bügling <boris@icculus.org>
2013-04-12 22:21:11 +0200

    Added Xcode project skeleton.

---
+++ src/TSAppDelegate.h
@@ -0,0 +1,7 @@
+//
+//  TSAppDelegate.h
+//  pebble-notifier
+//
+//  Created by Boris Bügling on 12.04.13.
+//  Copyright (c) 2013 Boris Bügling. All rights reserved.
+//
---
+++ src/main.m
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  pebble-notifier
+//
+//  Created by Boris Bügling on 12.04.13.
+//  Copyright (c) 2013 Boris Bügling. All rights reserved.
+//
+
+#include <CoreFoundation/CoreFoundation.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    CFShow(CFSTR("Hello, World!\n"));
+    return 0;
+}
